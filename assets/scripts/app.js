71e0b2db993116cc404d5002cab6d89927addf4c
VGaetano <gaetano.valentino@accenture.com>
2024-10-16 11:08:25 +0200

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,14 @@
+// import { apiKey, abc } from "./util.js";
+
+import * as util from "./util.js";
+
+// import { apiKey, abc as content } from ".util.js"; -->
+// console.log(content);
+
+// import apiKey from "./util.js";
+
+// console.log(apiKey, abc);
+
+// console.log(util);
+
+console.log(util.apiKey);
---
+++ assets/scripts/util.js
@@ -0,0 +1,4 @@
+export let apiKey = "qualcosa22";
+export let abc = "alfabeto";
+
+// export default "qualunque cosa haha 4";
